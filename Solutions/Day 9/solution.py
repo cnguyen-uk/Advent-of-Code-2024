d437b171b820668d6ecd6fbb45f56c009ec17272
cnguyen-uk <c.nguyen.uk@gmail.com>
2024-12-14 00:58:04 +0000

    Fixed part 1

--- Solutions/Day 9/solution.py
+++ Solutions/Day 9/solution.py
@@ -10,59 +10,59 @@ with open("input.txt") as input_txt:
 
 
 # Part 1
-def disk_map_to_blocks(disk_map: str) -> str:
-    blocks = ""
+def disk_map_to_blocks(disk_map: str) -> list[int | None]:
+    blocks = []
     id_number = 0
     for i, block_size in enumerate(disk_map):
         if i % 2:
-            block = "."
+            block = None
         else:
-            block = str(id_number)
+            block = int(id_number)
             id_number += 1
-        blocks += block * int(block_size)
+        blocks.extend([block] * int(block_size))
     return blocks
 
 
-def last_non_empty_file_block(blocks: str) -> dict[str, Any]:
+def last_non_empty_file_block(blocks: list[int | None]) -> dict[str, Any]:
     for i in reversed(range(len(blocks))):
-        if blocks[i] != ".":
+        if blocks[i] is not None:
             return {"position": i, "block_value": blocks[i]}
 
 
-def compactify(blocks: str) -> str:
-    compacted_blocks = ""
-    current_blocks = blocks
-    while current_blocks:
-        block = current_blocks[0]
-        if block != ".":
-            compacted_blocks += block
-        else:
-            block_to_move = last_non_empty_file_block(current_blocks)
-            if block_to_move:
-                i, block_value = block_to_move["position"], block_to_move["block_value"]
-                compacted_blocks += block_value
-                current_blocks = current_blocks[:i] + "." + current_blocks[i+1:]
-        current_blocks = current_blocks[1:]
-
-    remaining_free_space = len(blocks) - len(compacted_blocks)
-    compacted_blocks += "." * remaining_free_space
-    return compacted_blocks
+def first_empty_file_block_position(blocks: list[int | None]) -> int:
+    for i in range(len(blocks)):
+        if blocks[i] is None:
+            return i
 
 
-def checksum(blocks: str) -> int:
+def is_compacted(blocks: list[int | None]) -> bool:
+    first_none_position = first_empty_file_block_position(blocks)
+    for block in blocks[first_none_position:]:
+        if block is not None:
+            return False
+    return True
+
+
+def compactify(blocks: list[int | None]) -> list[int | None]:
+    while not is_compacted(blocks):
+        block_to_move = last_non_empty_file_block(blocks)
+        block_to_move_position, block_to_move_value = block_to_move["position"], block_to_move["block_value"]
+        blocks[block_to_move_position] = None
+        blocks[first_empty_file_block_position(blocks)] = block_to_move_value
+    return blocks
+
+
+def checksum(blocks: list[int | None]) -> int:
     total_checksum = 0
     for i, block in enumerate(blocks):
-        if block != ".":
-            total_checksum += i * int(block)
+        if block is not None:
+            total_checksum += i * block
     return total_checksum
 
 
 disk_map_blocks = disk_map_to_blocks(input_txt)
-print(disk_map_blocks)
 compacted_disk_map_blocks = compactify(disk_map_blocks)
-print(checksum(disk_map_blocks))
-print(compacted_disk_map_blocks)
-print(checksum(compacted_disk_map_blocks))  # Print: 90489586600
+print(checksum(compacted_disk_map_blocks))  # Print: 6332189866718
 
 
 # Part 2
